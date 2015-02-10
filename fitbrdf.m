c8d2e08c029a71638250ec7979c01f1e53525415
gzmk <gizemkucukoglu@gmail.com>
2015-02-10 11:57:34 -0500

    fit_brdf_v1
    Working version of fitbrdf with only ro_s, ro_d and lightColor
    variables. Need to add size and position as variables

--- fitbrdf.m
+++ fitbrdf.m
@@ -1,9 +1,6 @@
 %% 
 % Params for fminsearch scalexyz, position of the sphere, ro_s, ro_d, light
-% source
-banana = @(x)100*(x(2)-x(1)^2)^2+(1-x(1))^2;
 % Pass the function handle to fminsearch:
 
-[x,fval] = fminsearch(banana,[-1.2, 1])
-
-gloss70 = imread('DSC_0111_70gloss.pgm');
+start_params = [0.56; 0.56; 0.56];
+[var,fval, exitflag] = fminsearch(@(var) renderIm(var),start_params,optimset('MaxIter',10));
--- renderIm.m
+++ renderIm.m
@@ -1,47 +1,43 @@
 %fitting brdf to images
-% gloss70 = imread('DSC_0111_70gloss.pgm');
-% black = imread('DSC_0112.pgm');
-% 
-% imshow(gloss70)
-% 
-% gloss70_residual = gloss70-black';
 
 % Idea: I might be able to store the params in an array and write them into
 % the conditions file at each iteration - take a look at this
-function renderIm(ro_s, ro_d, lightColor)
+function costIm = renderIm(var)
 
-% params = [ro_s, ro_d, lightColor];
-params = [1 2 3];%test
-paramName = ['ro_s' 'ro_d' 'lightColor'];
+% titles of the parameters in the Conditions File
+param1 = 'ro_s'; 
+param2 = 'ro_d';
+param3 = 'lightColor';
 
-% dlmwrite('test_sphere2Conditions.txt',paramName, 'newline', 'unix')
-dlmwrite('test_sphere2Conditions.txt',params,'-append','delimiter',' ')
-type('test_sphere2Conditions.txt')
+sprintf('The new variables are: ro_s: %f ro_d: %f lightColor: %f', var(1), var(2), var(3))
 
-%% to write new conditions file with replaced params
+% these are going to be passed by the function
+ro_s = var(1);
+ro_d = var(2);
+lightColor = var(3);
+%% to write new conditions file with replaced params - always need to start with an initial conditions file
 % % open files
-% fidIn = fopen('test.txt');
-% fidOut = fopen('test2.txt','w');
-% 
-% % read in file
-% tline = fgets(fidIn);
-% index = 1; 
-% while ischar(tline)
-%     data{index} = tline;     
-%     tline = fgets(fid);         
-%     index = index + 1;
-% end
-% 
-% % replace first row
-% data{1,1} = 'Something else';
-% 
-% % write to second file
-% for l = data
-%     fprintf(fidOut,'%s\n', l{1,1});
-% end
-% 
-% fclose(fidIn);
-% fclose(fidOut);
+fidIn = fopen('test_sphereConditions.txt','r');
+
+% read in file
+tline = fgets(fidIn);% gets the title line so skip it
+tline = fgets(fidIn);% this is the line we want to replace with the params
+index = 1; 
+params_line{index} = tline
+
+fclose(fidIn);
+% replace first row
+str_params = sprintf('%.2f\t%.2f\t%.2f\t',ro_s, ro_d, lightColor);
+params_line{1,1} = str_params
+
+fidOut = fopen('test_sphereConditions.txt','w');
+str_title = sprintf('%s\t%s\t%s\t', param1, param2, param3)
+% write to second file
+fprintf(fidOut, '%s\n', str_title)
+fprintf(fidOut,'%s\n', params_line{1,1});
+
+
+fclose(fidOut);
 %% Rendering bit
 
 % Set preferences
@@ -59,22 +55,22 @@ addpath(genpath(pwd))
 
 %% Choose batch renderer options.
 
-% hints.imageWidth = 4012/2;
-% hints.imageHeight = 6034/2;
-hints.imageWidth = 600;% these are for quick rendering
-hints.imageHeight = 800;
+hints.imageWidth = 4012;
+hints.imageHeight = 6034;
+% hints.imageWidth = 600;% these are for quick rendering
+% hints.imageHeight = 800;
 hints.renderer = 'Mitsuba';
+
 datetime=datestr(now);
 datetime=strrep(datetime,':','_'); %Replace colon with underscore
 datetime=strrep(datetime,'-','_');%Replace minus sign with underscore
 datetime=strrep(datetime,' ','_');%Replace space with underscore
 hints.recipeName = ['Test-SphereFit' datetime];
 
-
 ChangeToWorkingFolder(hints);
 
-nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
-% nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
+% nativeSceneFiles = MakeSceneFiles(parentSceneFile, '', mappingsFile, hints);
+nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
 radianceDataFiles = BatchRender(nativeSceneFiles, hints);
 
 %comment all this out
@@ -84,15 +80,20 @@ montageName = sprintf('%s (%s)', 'Test-SphereFit', hints.renderer);
 montageFile = [montageName '.png'];
 [SRGBMontage, XYZMontage] = ...
     MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-% ShowXYZAndSRGB([], SRGBMontage, montageName);
 
-% imwrite(SRGBMontage, 'test_rendered70.png','BitDepth',16);
+% load the monochromatic image and display it
+imPath = ['/Local/Users/gizem/Documents/Research/GlossBump/Gloss_Level_Sphere_Photos/', hints.recipeName, '/renderings/Mitsuba/test_sphere-001.mat']
+load(imPath, 'multispectralImage');
+im2 = im2uint16(multispectralImage);
 
-%% Convert spectral image to sRGB:
-S = [400 10 31];
-[sRGBImage, XYZImage, rawRGBImage] = MultispectralToSRGB(multispectralImage, S, toneMapFactor, isScale);
-ShowXYZAndSRGB([], sRGBImage)
+%% calculate the ssd (error) between two images
+gloss70 = imread('DSC_0111_70gloss.pgm');
 
-gray70 = rgb2gray(sRGBImage);
-imshow(gray70)
-
+ssd = 0
+for i = 1:hints.imageHeight
+    for j = 1:hints.imageWidth
+        diff = gloss70(i,j) - im2(i,j);
+        ssd = ssd + diff^2;
+    end
+end
+costIm = ssd;
